f2fa0272edf713026c47756c6b55508e600e6595
lee_centos <1249776253@qq.com>
2019-09-18 18:20:50 +0800

    void lisp ok

---
+++ cmakeLearn/build/src/CMakeFiles/Test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Test.dir/Test1.cpp.o"
+  "CMakeFiles/Test.dir/VoidLisp.cpp.o"
+  "libTest.pdb"
+  "libTest.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
